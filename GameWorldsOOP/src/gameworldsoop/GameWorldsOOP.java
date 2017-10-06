388c07c0a9950026b51395c57d875bd681e0790b
Hartmann <Hartmann@DESKTOP-04JODUG>
2017-10-06 15:11:46 +0200

    Gruppe22

--- GameWorldsOOP/src/gameworldsoop/GameWorldsOOP.java
+++ GameWorldsOOP/src/gameworldsoop/GameWorldsOOP.java
@@ -6,6 +6,7 @@ public class GameWorldsOOP {
     
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Oscar");
     }
     
 }
