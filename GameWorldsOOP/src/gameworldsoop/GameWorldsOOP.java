71eb0749de8a746aafede328c4d4db41494c43cd
Hartmann <Hartmann@DESKTOP-04JODUG>
2017-10-09 15:20:10 +0200

    test

--- GameWorldsOOP/src/gameworldsoop/GameWorldsOOP.java
+++ GameWorldsOOP/src/gameworldsoop/GameWorldsOOP.java
@@ -6,16 +6,13 @@ public class GameWorldsOOP {
     
     public static void main(String[] args) {
         System.out.println("Hello World");
-<<<<<<< HEAD
-        System.out.println("Hello Oscar");
-=======
+
         System.out.println("Hvad sker der?");
         System.out.println("what up!");
         System.out.println("test");
         System.out.println("fedt fedt fedt det vriker sku");
+        System.out.println("Ændring");
         
-        
->>>>>>> be3467fb9d3578ee3acc14b6700e715920d0e188
     }
     
 }
