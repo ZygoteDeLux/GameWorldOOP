38378cce24d447800556ed6d1a24efa973770c68
Hartmann <Hartmann@DESKTOP-04JODUG>
2017-10-13 15:30:45 +0200

    Alle sad sammen - martin
    Problemer: MANGE

--- GameWorldsOOP/src/LemGame/Game.java
+++ GameWorldsOOP/src/LemGame/Game.java
@@ -1,24 +1,41 @@
 package LemGame;
+
 import java.util.*;
+
 public class Game {
 
+    Prop wallet = new Prop("Wallet", 25);
     private Room currentroom;
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name! ");
-        String insertName = input.nextLine();
-        Player playerName = new Player(insertName);
-        Prop wallet = new Prop("wallet", 25);
-        Prop beef = new Prop("beef", 25);
-        Prop cigarettes = new Prop("ciggarets", 25);
-        System.out.println(playerName.getPlayerName());
-        System.out.println(playerName.getPlayerCurrency());
-        playerName.addItem(beef);
-        playerName.addItem(wallet);
-        playerName.addItem(cigarettes);
-        playerName.showBag();
+        Game play = new Game();
+        play.play();
+        
     }
 
-    
+    public void createProps() {
+
+    }
+
+    public void createRoom() {
+        Room centrum = new Room("Centrum", 0);
+        centrum.addRoomItem(wallet);
+        centrum.removeRoomItem(wallet);
+        
+    }
+
+    public void play() {
+        while (true) {
+            
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your name! ");
+            String insertName = input.nextLine();
+            Player playerName = new Player(insertName);
+            System.out.println(playerName.getPlayerName());
+            System.out.println(playerName.getPlayerCurrency());
+            playerName.addItem(wallet);
+            playerName.showBag();
+            
+        }
+    }
 }
--- GameWorldsOOP/src/LemGame/Room.java
+++ GameWorldsOOP/src/LemGame/Room.java
@@ -1,5 +1,31 @@
 package LemGame;
+
 import java.util.*;
+
 public class Room {
-    int i;
+
+    String roomName;
+    int roomLocation;
+    String roomBehaviour;
+    ArrayList<Prop> roomItems = new ArrayList();
+
+    Room(String roomName, int roomLocation) {
+        this.roomName = roomName;
+        this.roomLocation = roomLocation;
+        
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+    public void addRoomItem(Prop prop){
+        roomItems.add(prop);
+    }
+    public void removeRoomItem(Prop prop){
+        roomItems.remove(prop);
+    }
+    public String getRoomItems(){
+        return roomItems.toString();
+    }
+    
 }
